0a142d3f38ac292f5d15df555a75b26f8410a4f7
jung-aaaaa <jung-aaaaa>
2022-10-07 16:21:15 +0900

    update

---
+++ app/src/main/java/com/example/mju_eclass/data/MyclassActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mju_eclass.data
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mju_eclass.R
+
+class MyclassActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_myclass)
+    }
+}
